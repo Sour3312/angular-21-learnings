1e66430b18b3f31422ba374bf877d1030041e048
sourabh.kumar3312@gmail.com <sourabh.kumar3312@gmail.com>
2026-01-11 19:21:02 +0530

    notes #1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('21');
+
+  name:WritableSignal<string> = signal<any>('');
+
+  setValue(value:any){
+    this.name.set(value);
+  }
+
+  reset(){
+    this.name.set('223')
+  }
+
+  //getter and setter
+  userName = signal('user');
+  student = signal({
+    name:'angular 18',
+    school:'google'
+  })
+
+  get uName(){
+    return this.userName();
+  }
+
+  set uName(val:string){
+    this.userName.set(val);
+  }
+
+  get details (){
+    return this.student().school;
+  }
+
+  set details(val:string){
+    this.student.update((item)=>({...item, school:val}))
+  }
 }
